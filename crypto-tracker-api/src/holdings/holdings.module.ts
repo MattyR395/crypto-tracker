7b9fdcdca0e767a6c8d0195ed0f75102adae8e6e
Matthew Risbey-Ward <matt.rw@outlook.com>
2022-08-24 00:22:52 +0100

    Auth guards for API

--- crypto-tracker-api/src/holdings/holdings.module.ts
+++ crypto-tracker-api/src/holdings/holdings.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HoldingsService } from './holdings.service';
 import { HoldingsController } from './holdings.controller';
+import { AuthzModule } from 'src/authz/authz.module';
 
 @Module({
+  imports: [AuthzModule],
   controllers: [HoldingsController],
   providers: [HoldingsService]
 })
